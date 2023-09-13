6a463e3828d015cab8ebdd4650a4914f02bea474
PerzotProgrammer <bartoszperz2007@gmail.com>
2023-09-13 09:12:18 +0000

    Dodano średniki i zmieniono button na input

--- 2Klasa/JSOkienka/script.js
+++ 2Klasa/JSOkienka/script.js
@@ -1,40 +1,40 @@
 function Alert()
 {
-    alert("KAZAKSTAN UGRAŻAJU NAM BAMBARDIEROWKE!")
+    alert("KAZAKSTAN UGRAŻAJU NAM BAMBARDIEROWKE!");
 } 
 
 function Confirm()
 {
-    let check = confirm("Czy jesteś pewien?")
-    if (check) alert(":)")
-    else alert(":(")
+    let check = confirm("Czy jesteś pewien?");
+    if (check) alert(":)");
+    else alert(":(");
 }
 
 function Prompt()
 {
-    let check = prompt("Cześć co tam!","Joł, hej, heloł")
-    if (check == null) alert("Czemu!!!")
-    else if (check == "Joł, hej, heloł") alert("Ale jesteś leniem :O")
-    else alert("To fajnie!")
+    let check = prompt("Cześć co tam!","Joł, hej, heloł");
+    if (check == null) alert("Czemu!!!");
+    else if (check == "Joł, hej, heloł") alert("Ale jesteś leniem :O");
+    else alert("To fajnie!");
 }
 
 function DodajOdejmij()
 {
-    let pierwsza = 2
-    let druga = "1"
-    let check = confirm("Dodaj lub odejmij")
-    if (check) alert(pierwsza + druga)
-    else alert(pierwsza - druga)
+    let pierwsza = 2;
+    let druga = "1";
+    let check = confirm("Dodaj lub odejmij");
+    if (check) alert(pierwsza + druga);
+    else alert(pierwsza - druga);
 }
 
 function Mnozenie()
 {
-    let pierwsza = 1
-    let druga = 1
-    let ile = 10
+    let pierwsza = 1;
+    let druga = 1;
+    let ile = 10;
     for ( let i = 0 ; i < ile; i ++) 
     {
-    alert("Iloczyn liczb " + pierwsza + " i " + druga + " wynosi " + (pierwsza * druga))
+    alert("Iloczyn liczb " + pierwsza + " i " + druga + " wynosi " + (pierwsza * druga));
     pierwsza ++;
     druga ++;
     }
@@ -42,5 +42,5 @@ function Mnozenie()
 
 function Banan()
 {
-    alert(('b' + 'a' + + 'a' + 'a').toLowerCase())
+    alert(('b' + 'a' + + 'a' + 'a').toLowerCase());
 }
