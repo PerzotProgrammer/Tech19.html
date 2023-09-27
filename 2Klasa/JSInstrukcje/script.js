a3767e69df93d43c314f753623a3587bc7edd5bd
PerzotProgrammer <bartoszperz2007@gmail.com>
2023-09-27 19:01:02 +0200

    Przesłano zadania z lekcji

--- 2Klasa/JSInstrukcje/script.js
+++ 2Klasa/JSInstrukcje/script.js
@@ -1,10 +1,31 @@
 function WalidujDane(){
     var login = document.getElementById("login").value;
     var password = document.getElementById("password").value;
-    if (login == "baranekbozy123" && password == "papiezpolak2137"){
+    var loginEnc = Szyfr(login);
+    var passwordEnc = Szyfr(password);
+
+    if (loginEnc == sessionStorage.getItem("login") && passwordEnc == sessionStorage.getItem("password")){
         location.href = "stronaBoga.html";
     }
     else{
         alert("Zły login lub hasło!");
     }
+}
+
+function DodajDane(){
+    var login = document.getElementById("login").value;
+    var password = document.getElementById("password").value;
+    
+    sessionStorage.setItem("login", Szyfr(login));
+    sessionStorage.setItem("password", Szyfr(password));
+}
+
+function Szyfr(szyfr){
+    var szyfrEnc = "";
+
+    for (var i = 0; i < szyfr.length; i++){
+        szyfrEnc += szyfr.charCodeAt(i);
+    }
+
+    return szyfrEnc;
 }
